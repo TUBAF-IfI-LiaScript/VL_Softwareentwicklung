e2de63e2fedb867f44e3c6a3a4f5a5eb9cd5c5e6
Yannik Höll <labruzzler@gmail.com>
2020-08-26 15:28:11 +0200

    modified pandoc cmd and preprocessing script

--- .github/workflows/preprocessing.py
+++ .github/workflows/preprocessing.py
@@ -20,6 +20,17 @@ for file in glob.glob("*.mdx"):
         filtered.append(line)
 
     with open(file, "w") as outfile:
+        if file.endswith("00_Einfuehrung.mdx"):  # insert metadata for pandoc in first md file
+            title = "C# Kurs TU Freiberg"
+            outfile.write("---\ntitle:\n  - {title}\nauthor:\n")
+            os.system("git shortlog -ns --all > authors.txt")
+            with open("authors.txt", "r") as authors:  # read in authors and write them into the yaml code
+                for line in authors:
+                    line = line.strip()
+                    name = line.split("\t")[1]
+                    outfile.write(f"  - {name}\n")
+
+            outfile.write("papersize:\n  - a4\ngeometry:\n  - margin=2cm\n---\n\\maketitle")
         outfile.write("".join(filtered))
 
 # pandoc  --toc --top-level-division=chapter -V geometry:margin=2cm --listings -H listings.tex -V linkcolor:blue --pdf-engine=xelatex -o doc.pdf *.mdx
