fa1f0caa8b20d836b075f77c816089c8796a5485
DEVensiv <35898914+DEVensiv@users.noreply.github.com>
2021-05-17 15:41:58 +0200

    Additional workflow for generating seperate pdfs for each script (#72)
    * modified preprocessing script + bash script to generate seperate pdfs
    
    * moved functinality into github workflow
    
    * fixed naming of new workflow
    
    * fixed run for conversion
    
    * fixed typo in workflow
    
    * fixed for loop in run
    
    * fixed file locations
    
    * added debug info
    
    * fixed another typo
    
    * fixed output directory

---
+++ .github/workflows/config/pre_multipdf.py
@@ -0,0 +1,33 @@
+import glob, os
+import shutil
+import re
+
+blackList = ["{{", "******", "@Rextester", "@Tau"]
+
+for file in glob.glob("*.md"):
+    if file == "README.md":
+        continue
+
+    content = open(file, 'r').readlines()
+    filtered = []
+    for line in content:
+        if any(entry in line for entry in blackList):
+            continue
+        result = re.search( r'```\S+', line)
+        if result:
+            line = result.group(0) + "\n"
+        filtered.append(line)
+
+    with open(file + "x", "w") as outfile:
+        title = "**Vorlesung Softwareentwicklung 2021**"
+        outfile.write(f"---\ntitle: |\n  {title}\n  https://github.com/SebastianZug/CsharpCourse\nauthor:\n")
+        with open(".github/workflows/config/authors.txt", "r") as authors:  # read in authors and write them into the yaml code
+            for line in authors:
+                name = line.strip()
+                outfile.write(f"  - {name}\n")
+
+        outfile.write("papersize:\n  - a4\ngeometry:\n  - margin=2cm\n")
+        outfile.write("header-includes:\n  - \\usepackage{titling}\n  - \\pretitle{\\begin{center}\n    \\includegraphics[width=2cm]{img/Logo_TU_Freiberg.png}\\LARGE\\\\}\n  - \\posttitle{\\end{center}}\n---\n")
+        outfile.write("".join(filtered))
+
+# pandoc  --toc --top-level-division=chapter -V geometry:margin=2cm --listings -H listings.tex -V linkcolor:blue --pdf-engine=xelatex -o doc.pdf *.mdx
