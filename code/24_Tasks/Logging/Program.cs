a6de503547d2ddd6fdb629c45cdaaf4258889b35
Sebastian Zug <sebastian.zug@informatik.tu-freiberg.de>
2022-07-05 12:04:01 +0200

    Update task examples

---
+++ code/24_Tasks/Logging/Program.cs
@@ -0,0 +1,32 @@
+﻿// Import NLog classes.
+using NLog;
+public static class Program
+{
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public static void Main()
+    {
+        var config = new NLog.Config.LoggingConfiguration();
+
+        // Targets where to log to: File and Console
+        var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "file.txt" };
+        var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+                    
+        // Rules for mapping loggers to targets            
+        config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+        config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+                    
+        // Apply config           
+        NLog.LogManager.Configuration = config;
+
+        try
+        {
+           Logger.Info("Hello world");
+        }
+        catch (Exception ex)
+        {
+           Logger.Error(ex, "Goodbye cruel world");
+        }
+    }
+}  
+
